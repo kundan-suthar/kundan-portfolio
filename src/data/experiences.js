2e1e5e62757a99c017f246749c039591b197603d
Kundan Suthar <kundanss22@gmail.com>
2024-09-11 19:30:16 +0530

    experince desc updated

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -6,7 +6,7 @@ const experiences = [
         duration: "May-23 - Present",
         jobRole: "Frontend Developer",
         place: "Remote",
-        jobDesc: ' A Real time movie app to showcase all latest movies with their ratings. A Real time movie app to showcase all latest movies with their ratings.',
+        jobDesc: ' At New Era Tech, I have been responsible for supporting and resolving client-reported issues on existing projects, ensuring smooth functionality and user experience. I worked on developing new solutions using the SharePoint API and contributed to various React projects, building dynamic user interfaces. My role also involved collaborating with cross-functional teams to deliver robust and high-quality frontend solutions.',
         skills: ["React", "Redux", "Tailwind", "Sharepoint", "Bootstrap", "JQuery"],
 
     },
@@ -16,7 +16,7 @@ const experiences = [
         duration: "April-22 - Jan-23",
         jobRole: "Frontend Developer",
         place: "PUNE",
-        jobDesc: ' A Real time movie app to showcase all latest movies with their ratings.',
+        jobDesc: 'During my time at Inought Sportech, I gathered client requirements, proposed solutions, and developed frontend systems that met their needs. I was responsible for building user interfaces based on design guidelines, integrating APIs to ensure seamless backend-frontend communication, and optimizing existing code for better performance. Additionally, I played a key role in improving the user experience through thoughtful UI/UX enhancements.',
         skills: ["React", "Redux", "SCSS"],
 
     },
--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     {
         id: 1,
         title: 'Movix',
-        description: ' At New Era Tech, I have been responsible for supporting and resolving client-reported issues on existing projects, ensuring smooth functionality and user experience. I worked on developing new solutions using the SharePoint API and contributed to various React projects, building dynamic user interfaces. My role also involved collaborating with cross-functional teams to deliver robust and high-quality frontend solutions.',
+        description: 'A Real time movie app to showcase all latest movies with their ratings. A Real time movie app to showcase all latest movies with their ratings.',
         imageUrl: Project1,
         gitUrl: "https://github.com/kk240/MovieDB",
         liveUrl: "https://cinematechs.vercel.app/",
@@ -15,7 +15,7 @@ const projects = [
     {
         id: 1,
         title: 'know your universe',
-        description: 'During my time at Inought Sportech, I gathered client requirements, proposed solutions, and developed frontend systems that met their needs. I was responsible for building user interfaces based on design guidelines, integrating APIs to ensure seamless backend-frontend communication, and optimizing existing code for better performance. Additionally, I played a key role in improving the user experience through thoughtful UI/UX enhancements.',
+        description: ' A Real time movie app to showcase all latest movies with their ratings.',
         imageUrl: Project2,
         gitUrl: "https://github.com/kk240/NasaExplorer",
         liveUrl: "https://knowyouruniverse.vercel.app/",
